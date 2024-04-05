b5b210279710969b9226e5ad64c7c6e8bbb3884d
Eduarbossa <160442349+Eduarbossa@users.noreply.github.com>
2024-04-04 22:00:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Hola , bienvenidos a la calculadora");
+var numero1 =parseInt(prompt("por favor ingresa un numero del 0 al infinito"));
+alert("elegistes el numero" + numero1);
+var numero2 = parseInt(prompt("por favor ingresa un segundo numero del 0 al infinito"));
+alert("elegistes un segundo numero" + numero2 );
+
+
